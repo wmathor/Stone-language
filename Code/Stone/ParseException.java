50b2c9d8ab352b615d799fc1da3c4285e82a7094
mathor <32392878+mathors@users.noreply.github.com>
2018-12-14 21:38:56 +0800

    Create ParseException.java

---
+++ Code/Stone/ParseException.java
@@ -0,0 +1,28 @@
+package Stone;
+
+import java.io.IOException;
+
+public class ParseException extends Exception {
+	public ParseException(Token t) {
+		this("", t);
+	}
+
+	public ParseException(String msg, Token t) {
+		super("syntax error around " + location(t) + ". " + msg);
+	}
+
+	private static String location(Token t) {
+		if (t == Token.EOF)
+			return "the last line";
+		else
+			return "\"" + t.getText() + "\" at line " + t.getLineNumber();
+	}
+
+	public ParseException(IOException e) {
+		super(e);
+	}
+
+	public ParseException(String msg) {
+		super(msg);
+	}
+}
