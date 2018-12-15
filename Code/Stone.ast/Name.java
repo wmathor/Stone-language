b4ea6b4356b2983ec5b63c0918ef3f2da2f485f0
mathor <32392878+mathors@users.noreply.github.com>
2018-12-15 14:26:51 +0800

    Create Name.java

---
+++ Code/Stone.ast/Name.java
@@ -0,0 +1,14 @@
+package Stone.ast;
+
+import Stone.Token;
+
+public class Name extends ASTLeaf {
+
+	public Name(Token t) {
+		super(t);
+	}
+
+	public String name() {
+		return token().getText();
+	}
+}
