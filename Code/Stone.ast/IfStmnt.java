063661545356b4914de232dddef5c438760f394f
mathor <32392878+mathors@users.noreply.github.com>
2018-12-15 21:29:35 +0800

    Create IfStmnt.java

---
+++ Code/Stone.ast/IfStmnt.java
@@ -0,0 +1,25 @@
+package Stone.ast;
+import java.util.List;
+
+public class IfStmnt extends ASTList {
+
+	public IfStmnt(List<ASTree> t) {
+		super(t);
+	}
+
+	public ASTree condition() {
+		return child(0);
+	}
+
+	public ASTree thenBlock() {
+		return child(1);
+	}
+
+	public ASTree elseBlock() {
+		return numChildren() > 2 ? child(2) : null;
+	}
+
+	public String toString() {
+		return "(if " + condition() + " " + thenBlock() + " else " + elseBlock() + ")";
+	}
+}
